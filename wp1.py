b59155fc3db14aafdba0762f35bf0db1620b7880
Alisha Mishra <alishm1@uci.edu>
2024-01-29 12:28:51 -0800

    try reading input file and create reversed text

--- wp1.py
+++ wp1.py
@@ -8,6 +8,23 @@ def main():
         print("Incorrect input format")
         return
 
-    #input_filename = sys.argv[1]
-    #output_filename = sys.argv[2]
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    try:
+        with open(input_filename, 'r', encoding='utf-8') as input_file:
+            input_text = input_file.read()
+
+        if not input_text:
+            print("Error: The input file is empty.")
+            return
+
+        encrypted_text = reverse_cipher(input_text)
+
+        with open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write(encrypted_text)
+
+        print("Encryption completed. Check", output_filename)
+
+
 
